8e8f9d9a8f5df75453b6d34d551c62210aad7a93
Muhammad Ejaz <mejaz4349@gmail.com>
2022-06-23 03:54:00 +0500

    complete assigment

---
+++ app.js
@@ -0,0 +1,32 @@
+let quote = document.getElementById('quote');
+let author = document.getElementById('author');
+let qbtn = document.getElementById('btn');
+qbtn.addEventListener('click', majorF);
+let loader = document.getElementById('loader');
+
+function majorF(){
+    loaderf();
+    setTimeout(getQuote, 2000);
+    setTimeout(loaderf1, 2000);
+}
+
+
+function loaderf(){
+    loader.style.display = "block";
+    document.getElementById('check').style.display = "none";
+}
+function loaderf1(){
+    loader.style.display = "none";
+    document.getElementById('check').style.display = "block";
+}
+function getQuote(){
+   let newp =  fetch('https://dummyjson.com/quotes/random')
+    .then( res => res.json())
+    .then(json=>{
+        let quotetxt = json.quote;
+        let authortxt = json.author;
+        quote.innerHTML = `"${quotetxt}"`;
+        author.innerHTML = `"${authortxt}"`;
+    });
+}
+
